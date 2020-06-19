096ba68f4a4265439f861a3aa57bbffdc8b47afb
Glitch (pato-fcc-file-metadata-microservice) <none>
2020-06-19 20:22:09 +1200

    Initial commit

--- server.js
+++ server.js
@@ -1,23 +1,46 @@
-'use strict';
+"use strict";
 
-var express = require('express');
-var cors = require('cors');
-
-// require and use "multer"...
-
+var express = require("express");
+var cors = require("cors");
+var multer = require("multer");
 var app = express();
+var upload = multer({ dest: "uploads/" });
 
 app.use(cors());
-app.use('/public', express.static(process.cwd() + '/public'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
 
-app.get('/', function (req, res) {
-     res.sendFile(process.cwd() + '/views/index.html');
-  });
+app.use("/public", express.static(process.cwd() + "/public"));
 
-app.get('/hello', function(req, res){
-  res.json({greetings: "Hello, API"});
+app.get("/", function(req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
-app.listen(process.env.PORT || 3000, function () {
-  console.log('Node.js listening ...');
+app.post("/api/fileanalyse", upload.single("upfile"), async (req, res) => {
+  try {
+    if (!req.file) {
+      res.send({
+        status: false,
+        message: "No file uploaded"
+      });
+    } else {
+      //Use the name of the input field (i.e. "avatar") to retrieve the uploaded file
+      let file = req.file;
+
+      //Use the mv() method to place the file in upload directory (i.e. "uploads")
+      // avatar.mv('./uploads/' + avatar.name);
+
+      //send response
+      res.json({
+        name: file.originalname,
+        size: file.size
+      });
+    }
+  } catch (err) {
+    res.status(500).send(err);
+  }
+});
+
+app.listen(process.env.PORT || 3000, function() {
+  // console.log('Node.js listening ...');
 });
